8866280d310b05f5522254f19575d65ee1e5ef0d
FamousDirector <jamesadcameron111@gmail.com>
2016-12-23 10:37:00 -0400

    created controller data table

---
+++ DatabaseV1/DatabaseV1/ControllerDataTable.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ControllerDataTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UID] NVARCHAR(50) NOT NULL, 
+    [RelayState] INT NOT NULL, 
+    [Temperature] INT NOT NULL, 
+    [Power] INT NOT NULL, 
+    [EntryTime] DATETIME2 NOT NULL DEFAULT getdate()
+)

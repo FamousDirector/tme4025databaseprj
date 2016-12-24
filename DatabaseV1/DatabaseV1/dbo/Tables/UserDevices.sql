76e0934d2aff8a03230553499b7dca4a915be265
FamousDirector <jamesadcameron111@gmail.com>
2016-12-24 18:59:53 -0400

    Added new table to store User Device Associations

---
+++ DatabaseV1/DatabaseV1/dbo/Tables/UserDevices.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[UserDevices]
+(
+	[Id] INT NOT NULL IDENTITY (1,1) PRIMARY KEY, 
+    [UserName] NVARCHAR(256) NOT NULL, 
+    [UID] NVARCHAR(50) NOT NULL, 
+    [DeviceName] NVARCHAR(256) NOT NULL 
+)

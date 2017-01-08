4fa6fbb7a0d7dbf5b86fe1bb9b20563a90eca208
FamousDirector <jamesadcameron111@gmail.com>
2017-01-08 16:42:03 -0400

    changed schedule to use an int
    this is so it can store negative seconds if need be

--- DatabaseV1/DatabaseV1/dbo/Tables/DeviceSchedules.sql
+++ DatabaseV1/DatabaseV1/dbo/Tables/DeviceSchedules.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL IDENTITY (1,1) PRIMARY KEY, 
     [UID] NVARCHAR(50) NOT NULL UNIQUE, 
-    [ScheduledOnTime] DATETIME NULL,
-    [ScheduledOffTime] DATETIME NULL
+    [ScheduledOnTimeSeconds] INT NULL,
+    [ScheduledOffTimeSeconds] INT NULL
 
 )

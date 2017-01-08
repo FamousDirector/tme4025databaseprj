576110141ea0f4c27e3086c46ea311e8ad887a55
FamousDirector <jamesadcameron111@gmail.com>
2017-01-08 19:47:32 -0400

    remove timezone table
    timezone will be grabbed from the users cookie

--- DatabaseV1/DatabaseV1/dbo/Tables/UserDevices.sql
+++ DatabaseV1/DatabaseV1/dbo/Tables/UserDevices.sql
@@ -3,7 +3,6 @@
 	[Id] INT NOT NULL IDENTITY (1,1) PRIMARY KEY, 
     [UserName] NVARCHAR(256) NOT NULL, 
     [UID] NVARCHAR(50) NOT NULL UNIQUE, 
-    [DeviceName] NVARCHAR(256) NOT NULL, 
-    [TimeZoneRegion] NCHAR(10) NOT NULL 
+    [DeviceName] NVARCHAR(256) NOT NULL
 
 )

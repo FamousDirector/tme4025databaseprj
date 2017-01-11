416556b66d89fc389c766e0e2287dccbfe7d9ff1
FamousDirector <jamesadcameron111@gmail.com>
2017-01-11 19:32:17 -0400

    changed default get time to utc
    ensures times are in UTC

--- DatabaseV1/DatabaseV1/dbo/Tables/ControllerDataTable.sql
+++ DatabaseV1/DatabaseV1/dbo/Tables/ControllerDataTable.sql
@@ -5,5 +5,5 @@
     [RelayState] INT NOT NULL, 
     [Temperature] INT NOT NULL, 
     [Power] INT NOT NULL, 
-    [EntryTime] DATETIME2 NOT NULL DEFAULT getdate()
+    [EntryTime] DATETIME2 NOT NULL DEFAULT GETUTCDATE()
 )
